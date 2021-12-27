6c45ce3ffe757510fa94a1ceb380f8fcb35ad1cb
Sujal Duwa <sujalduwa@gmail.com>
2021-12-27 22:11:51 +0545

    completed ball collision

--- js-assignments/assignment-3/src/script.js
+++ js-assignments/assignment-3/src/script.js
@@ -1,58 +1,119 @@
-const container = document.getElementById('container');
+// Global constants used.
+const CONTAINER_WIDTH = 1200;
+const CONTAINER_HEIGHT = 600;
+const ACCELERATION_CONSTANT = 20;
+const ACCELERATION_LOSS_CONSTANT = 200;
+const SPEED_CONSTANT = 50;
 
-const containerWidth = 1200;
-const containerHeight = 600;
-const fps = 60;
-const ballCount = 20;
+// To identify each ball object with an index.
 let ballIndex = 0;
 
+/**
+ * Represents a ball.
+ */
 class Ball {
+  /**
+   * @param {number} radius - the radius of the ball.
+   */
   constructor(radius) {
     this.ballIndex = ballIndex;
-    // console.log(ballIndex);
     ballIndex++;
     this.ball = document.createElement('div');
-    // this.ball.innerHTML = `${this.ballIndex - 1}`;
     this.ball.classList.add('ball');
     this.radius = radius;
+
+    // Assumed mass to be same as radius.
     this.mass = this.radius;
+
+    // Initial acceleration is 0.
+    this.acceleration = 0;
+
+    // Width and height is double of the radius.
     this.ballWidth = this.radius * 2;
     this.ballHeight = this.radius * 2;
     this.ball.style.width = this.radius * 2 + 'px';
     this.ball.style.height = this.radius * 2 + 'px';
-    this.x = getRandomInt(0, containerWidth - this.ballWidth);
-    this.y = getRandomInt(0, containerHeight - this.ballHeight);
+    this.x = getRandomInt(0, CONTAINER_WIDTH - this.ballWidth);
+    this.y = getRandomInt(0, CONTAINER_HEIGHT - this.ballHeight);
+
+    // Gets random direction (positive or negative) in x and y axis for the ball to initially move.
     this.dx = getDirection();
     this.dy = getDirection();
     this.ball.style.position = 'absolute';
     this.ball.style.left = this.y + 'px';
     this.ball.style.top = this.x + 'px';
-    this.xSpeed = 30 / this.mass;
-    this.ySpeed = 30 / this.mass;
+    this.ball.style.backgroundColor = getRandomColor();
+
+    // I have used getRandomFloat to move the balls in random angles.
+    // Speed is inversely proportional to the mass of the ball.
+    this.xSpeed = (SPEED_CONSTANT / this.mass) * getRandomFloat(0.1, 1);
+    this.ySpeed = (SPEED_CONSTANT / this.mass) * getRandomFloat(0.1, 1);
   }
 
+  /**
+   * Appends the ball div to the container.
+   */
   draw() {
     container.appendChild(this.ball);
   }
 
+  /**
+   * Checks the randomly assigned initial position of balls and if they co-incide,
+   * then it assigns the ball to new position.
+   */
+  checkInitialPosition() {
+    ballArray.forEach((ball) => {
+      if (this.ballIndex !== ball.ballIndex) {
+        let distanceX = this.x + this.radius - (ball.x + ball.radius);
+        let distanceY = this.y + this.radius - (ball.y + ball.radius);
+
+        // Distance formula.
+        let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+        if (distance < this.radius + ball.radius) {
+          this.x = getRandomInt(0, CONTAINER_WIDTH - this.ballWidth);
+          this.y = getRandomInt(0, CONTAINER_HEIGHT - this.ballHeight);
+          this.radius = 10;
+          this.mass = this.radius;
+          this.ballWidth = this.radius * 2;
+          this.ballHeight = this.radius * 2;
+          this.ball.style.width = this.radius * 2 + 'px';
+          this.ball.style.height = this.radius * 2 + 'px';
+          this.xSpeed = SPEED_CONSTANT / this.mass;
+          this.ySpeed = SPEED_CONSTANT / this.mass;
+        }
+      }
+    });
+  }
+
+  /**
+   * Changes the position of balls inside the container by running it in 60fps.
+   */
   move() {
-    this.x += this.xSpeed * this.dx;
-    this.y += this.ySpeed * this.dy;
+    this.x += (this.xSpeed + this.acceleration) * this.dx;
+    this.y += (this.ySpeed + this.acceleration) * this.dy;
     this.ball.style.top = this.y + 'px';
     this.ball.style.left = this.x + 'px';
-    this.checkBallCollision();
+
+    // accelereation gets lost slowly in approximately 3.33 secs (200/60)
+    if (this.acceleration != 0) {
+      this.acceleration -= this.acceleration / ACCELERATION_LOSS_CONSTANT;
+    }
+
     this.checkWallCollision();
-    // this.checkStuck();
-    // this.checkBallCollision();
+    this.checkBallCollision();
     window.requestAnimationFrame(this.move.bind(this));
   }
 
+  /**
+   * Checks ball for any wall collision and changes direction if there is collision.
+   */
   checkWallCollision() {
-    if (this.x > containerWidth - this.ballWidth) {
+    if (this.x > CONTAINER_WIDTH - this.ballWidth) {
       this.dx = -1;
     }
 
-    if (this.y > containerHeight - this.ballHeight) {
+    if (this.y > CONTAINER_HEIGHT - this.ballHeight) {
       this.dy = -1;
     }
 
@@ -65,91 +126,41 @@ class Ball {
     }
   }
 
-  checkInitialPosition() {
-    ballArray.forEach((ball) => {
-      if (this.ballIndex !== ball.ballIndex) {
-        let distanceX = this.x + this.radius - (ball.x + ball.radius);
-        let distanceY = this.y + this.radius - (ball.y + ball.radius);
-        let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-
-        if (distance < this.radius + ball.radius) {
-          this.x = getRandomInt(0, containerWidth - this.ballWidth);
-          this.y = getRandomInt(0, containerHeight - this.ballHeight);
-          // this.ball.style.width = '100px';
-          // this.ball.style.height = '100px';
-          // console.log('i ran');
-
-          this.radius = 10;
-          this.mass = this.radius;
-          this.ballWidth = this.radius * 2;
-          this.ballHeight = this.radius * 2;
-          this.ball.style.width = this.radius * 2 + 'px';
-          this.ball.style.height = this.radius * 2 + 'px';
-          this.xSpeed = 30 / this.mass;
-          this.ySpeed = 30 / this.mass;
-
-          // container.removeChild(this.ball);
-          // new Ball(getRandomInt(10, 41));
-          // this.dx = 1;
-          // ball.dx = -1;
-          // this.dy = 1;
-          // ball.dy = -1;
-        } else {
-          // no collision
-        }
-      }
-    });
-  }
-
+  /**
+   * Checks for any ball to ball collision and changes direction accordingly.
+   */
   checkBallCollision() {
     ballArray.forEach((ball) => {
       if (this.ballIndex !== ball.ballIndex) {
         let distanceX = this.x + this.radius - (ball.x + ball.radius);
         let distanceY = this.y + this.radius - (ball.y + ball.radius);
+
+        // Distance formula.
         let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
 
         if (distance < this.radius + ball.radius) {
           this.dx = changeDirection(this.dx);
           this.dy = changeDirection(this.dy);
-          // ball.dx = changeDirection(ball.dx);
-          // ball.dy = changeDirection(ball.dy);
-        } else {
-          // no collision
+
+          // Acceleration is inversely proportional to the mass of ball.
+          this.acceleration = ACCELERATION_CONSTANT / this.mass;
         }
       }
     });
   }
-
-  // checkStuck() {
-  //   ballArray.forEach((ball) => {
-  //     if (this.ballIndex !== ball.ballIndex) {
-  //       let distanceX = this.x + this.radius - (ball.x + ball.radius);
-  //       let distanceY = this.y + this.radius - (ball.y + ball.radius);
-  //       let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-
-  //       if (distance < this.radius + ball.radius) {
-  //         this.dx = 1;
-  //         ball.dx = -1;
-  //         // this.dy = changeDirection(this.dy);
-  //         // ball.dx = changeDirection(ball.dx);
-  //         // ball.dy = changeDirection(ball.dy);
-  //       } else {
-  //         // no collision
-  //       }
-  //     }
-  //   });
-  // }
 }
 
+const container = document.getElementById('container');
+const ballCount = 20;
 const ballArray = [];
+
+/**
+ * Generates number of ball objects as specified in the ballCount.
+ */
 for (let i = 0; i < ballCount; i++) {
-  const ball = new Ball(getRandomInt(10, 41));
-  // const ball = new Ball(10);
+  const ball = new Ball(getRandomInt(10, 31));
   ball.draw();
   ball.checkInitialPosition();
   ball.move();
   ballArray.push(ball);
-  // ball.checkBallCollision();
 }
-// console.log(ballArray);
-// console.log(ballArray);
--- js-assignments/assignment-3/src/utils.js
+++ js-assignments/assignment-3/src/utils.js
@@ -1,14 +1,78 @@
+/**
+ * Returns a random integer from a given range.
+ * @param {number} min - minimum value (inclusive)
+ * @param {number} max - maximum value (exclusive)
+ * @returns - a random integer between min and max.
+ */
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
+
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+/**
+ * Returns 1 or -1 value randomly.
+ * @returns 1 or -1 value randomly.
+ */
 function getDirection() {
   return Math.random() > 0.5 ? 1 : -1;
 }
 
+/**
+ * Returns value that represent opposite direction.
+ * @param {number} direction - positive or negative direction.
+ * @returns - returns opposite of input direction.
+ */
 function changeDirection(direction) {
   if (direction === -1) return 1;
-  else if (direction === 1) return -1;
+
+  return -1;
+}
+
+/**
+ * Generates a random float value in a given range.
+ * @param {number} min - minimum float value (inclusive)
+ * @param {number} max - maximum float value (exclusive)
+ * @returns - a float value between min and max.
+ */
+function getRandomFloat(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+/**
+ * Generates random color from an array.
+ * @returns - a random hexcode of color from colors array.
+ */
+function getRandomColor() {
+  const colors = [
+    '#85144b',
+    '#000000',
+    '#c0c0c0',
+    '#808080',
+    '#008000',
+    '#00ff00',
+    '#808000',
+    '#000080',
+    '#0000ff',
+    '#008080',
+    '#00ffff',
+    '#7fffd4',
+    '#8a2be2',
+    '#a52a2a',
+    '#5f9ea0',
+    '#7fff00',
+    '#d2691e',
+    '#6495ed',
+    '#dc143c',
+    '#00ffff',
+    '#00008b',
+    '#008b8b',
+    '#b8860b',
+    '#a9a9a9',
+    '#006400',
+  ];
+  var randColor = colors[Math.floor(Math.random() * colors.length)];
+
+  return randColor;
 }
